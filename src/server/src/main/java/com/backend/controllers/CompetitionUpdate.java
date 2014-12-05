78d99aa204e6fa09d0614a62067f75d2cc79adf2
frederic.joanis <frederic.joanis@gmail.com>
2014-12-05 10:09:57 -0500

    Competition ranking is being updated properly in the database when moving it in the UI.

--- src/server/src/main/java/com/backend/controllers/CompetitionUpdate.java
+++ src/server/src/main/java/com/backend/controllers/CompetitionUpdate.java
@@ -1,7 +1,7 @@
 package com.backend.controllers;
 
 import java.io.IOException;
-import java.util.Enumeration;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.bson.types.ObjectId;
+
+import com.backend.models.Competition;
+import com.backend.models.School;
 import com.framework.models.Essentials;
 
 @WebServlet("/admin/competitionUpdate")
@@ -21,20 +25,47 @@ public class CompetitionUpdate extends HttpServlet
 	{
 		try(Essentials essentials = Essentials.createEssentials(request,  response))
 		{
-			for (Enumeration<String> iter = request.getParameterNames(); iter.hasMoreElements(); ) 
+			Competition competition = Competition.get(essentials);
+			ArrayList<School> toUpdate = new ArrayList<School>();
+
+			for (String id : request.getParameterValues("id[]"))
 			{
-			    System.out.println(iter.nextElement());
+			    toUpdate.add(essentials.database.findOne(School.class, new ObjectId(id)));
 			}
 			
+			Competition updatedCompetition = null;
+
 			for (String string : request.getParameterValues("arrayId")) 
 			{
-			    System.out.println(string);
+				switch(string)
+				{
+				case "kiosk":
+					updatedCompetition = new Competition(competition._id, toUpdate, competition.programming, competition.robotConstruction, competition.robotDesign, competition.sportsmanship, competition.video, competition.websiteDesign, competition.websiteJournalism);
+					break;
+				case "programming":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, toUpdate, competition.robotConstruction, competition.robotDesign, competition.sportsmanship, competition.video, competition.websiteDesign, competition.websiteJournalism);
+					break;
+				case "robotConstruction":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, competition.programming, toUpdate, competition.robotDesign, competition.sportsmanship, competition.video, competition.websiteDesign, competition.websiteJournalism);
+					break;
+				case "robotDesign":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, competition.programming, competition.robotConstruction, toUpdate, competition.sportsmanship, competition.video, competition.websiteDesign, competition.websiteJournalism);
+					break;
+				case "sportsmanship":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, competition.programming, competition.robotConstruction, competition.robotDesign, toUpdate, competition.video, competition.websiteDesign, competition.websiteJournalism);
+					break;
+				case "video":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, competition.programming, competition.robotConstruction, competition.robotDesign, competition.sportsmanship, toUpdate, competition.websiteDesign, competition.websiteJournalism);
+					break;
+				case "websiteDesign":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, competition.programming, competition.robotConstruction, competition.robotDesign, competition.sportsmanship, competition.video, toUpdate, competition.websiteJournalism);
+					break;
+				case "websiteJournalism":
+					updatedCompetition = new Competition(competition._id, competition.kiosk, competition.programming, competition.robotConstruction, competition.robotDesign, competition.sportsmanship, competition.video, competition.websiteDesign, toUpdate);
+					break;
+				}
 			}
-			
-			for (String string : request.getParameterValues("id[]")) 
-			{
-			    System.out.println(string);
-			}
+			essentials.database.save(updatedCompetition);
 		}
 	}
 }
